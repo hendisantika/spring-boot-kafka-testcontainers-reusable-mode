4fb1a0697c4f34756c5d2d1b0bdcdf07cbe3a6ac
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 19:35:10 +0700

    fix: Integration Test🫸🌀✏️📗🐧🐳

--- consumer/src/test/java/id/my/hendisantika/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/id/my/hendisantika/consumer/ConsumerApplicationTests.java
@@ -1,6 +1,17 @@
 package id.my.hendisantika.consumer;
 
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.Network;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+
+import java.util.List;
 
 class ConsumerApplicationTests {
 
@@ -10,4 +21,51 @@ class ConsumerApplicationTests {
                 .run(args);
     }
 
+    @TestConfiguration(proxyBeanMethods = false)
+    static class ContainerConfiguration {
+
+        private static final String KAFKA_NETWORK = "kafka-network";
+
+        Network network = getNetwork();
+
+        static Network getNetwork() {
+            Network defaultDaprNetwork = new Network() {
+                @Override
+                public String getId() {
+                    return KAFKA_NETWORK;
+                }
+
+                @Override
+                public void close() {
+
+                }
+
+                @Override
+                public Statement apply(Statement base, Description description) {
+                    return null;
+                }
+            };
+
+            List<com.github.dockerjava.api.model.Network> networks = DockerClientFactory.instance().client().listNetworksCmd().withNameFilter(KAFKA_NETWORK).exec();
+            if (networks.isEmpty()) {
+                Network.builder()
+                        .createNetworkCmdModifier(cmd -> cmd.withName(KAFKA_NETWORK))
+                        .build().getId();
+                return defaultDaprNetwork;
+            } else {
+                return defaultDaprNetwork;
+            }
+        }
+
+        @Bean
+        @ServiceConnection
+        @RestartScope
+        ConfluentKafkaContainer kafkaContainer() {
+            return new ConfluentKafkaContainer("confluentinc/cp-kafka:7.4.0")
+                    .withListener("kafka:19092")
+                    .withNetwork(network)
+                    .withReuse(true);
+        }
+
+    }
 }
