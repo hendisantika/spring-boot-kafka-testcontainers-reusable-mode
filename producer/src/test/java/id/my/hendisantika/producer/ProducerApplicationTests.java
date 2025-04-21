8d9855898a49d9c5b96397c4e4712f22cd02cd52
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 19:06:51 +0700

    fet: Add psvm🫸🌀✏️📗🐧🐳

--- producer/src/test/java/id/my/hendisantika/producer/ProducerApplicationTests.java
+++ producer/src/test/java/id/my/hendisantika/producer/ProducerApplicationTests.java
@@ -1,13 +1,14 @@
 package id.my.hendisantika.producer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
 class ProducerApplicationTests {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.from(ProducerApplication::main)
+                .with(ContainerConfiguration.class)
+                .run(args);
     }
+
 
 }
