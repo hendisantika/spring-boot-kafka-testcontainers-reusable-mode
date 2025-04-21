515f8b96941c98808caed2982bb3bc599812cd8a
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 19:06:09 +0700

    fet: Add EnableScheduling🫸🌀✏️📗🐧🐳

--- producer/src/main/java/id/my/hendisantika/producer/ProducerApplication.java
+++ producer/src/main/java/id/my/hendisantika/producer/ProducerApplication.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+
+@EnableScheduling
 @SpringBootApplication
 public class ProducerApplication {
 
@@ -10,4 +17,11 @@ public class ProducerApplication {
         SpringApplication.run(ProducerApplication.class, args);
     }
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 1000)
+    void produceMessage() {
+        this.kafkaTemplate.send("test-topic", "test-data " + LocalDateTime.now());
+    }
 }
