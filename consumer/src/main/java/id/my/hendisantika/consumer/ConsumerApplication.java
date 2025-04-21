c7078462366537fd3202aef8b8372f6deaffd519
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 19:29:41 +0700

    feat: Add listener🫸🌀✏️📗🐧🐳

--- consumer/src/main/java/id/my/hendisantika/consumer/ConsumerApplication.java
+++ consumer/src/main/java/id/my/hendisantika/consumer/ConsumerApplication.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.consumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@Slf4j
 @SpringBootApplication
 public class ConsumerApplication {
 
@@ -10,4 +13,8 @@ public class ConsumerApplication {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    void listener(String message) {
+        log.info("Received Message in group foo: {}", message);
+    }
 }
