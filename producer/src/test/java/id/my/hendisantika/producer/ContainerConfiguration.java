a2295b2d9658830698d1da63dee836bae119766b
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 19:08:54 +0700

    fet: Add DockerClientFactory🫸🌀✏️📗🐧🐳

--- producer/src/test/java/id/my/hendisantika/producer/ContainerConfiguration.java
+++ producer/src/test/java/id/my/hendisantika/producer/ContainerConfiguration.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.producer;
 
+import org.junit.runner.Description;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.Network;
+import org.testcontainers.images.builder.dockerfile.statement.Statement;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,33 @@ public class ContainerConfiguration {
     private static final String KAFKA_NETWORK = "kafka-network";
 
     Network network = getNetwork();
+
+    static Network getNetwork() {
+        Network defaultDaprNetwork = new Network() {
+            @Override
+            public String getId() {
+                return KAFKA_NETWORK;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public Statement apply(Statement base, Description description) {
+                return null;
+            }
+        };
+
+        List<Network> networks = DockerClientFactory.instance().client().listNetworksCmd().withNameFilter(KAFKA_NETWORK).exec();
+        if (networks.isEmpty()) {
+            Network.builder()
+                    .createNetworkCmdModifier(cmd -> cmd.withName(KAFKA_NETWORK))
+                    .build().getId();
+            return defaultDaprNetwork;
+        } else {
+            return defaultDaprNetwork;
+        }
+    }
 }
